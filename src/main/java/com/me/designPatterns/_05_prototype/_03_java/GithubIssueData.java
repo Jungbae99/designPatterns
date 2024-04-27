bbcf8dad36a46ae2a2f1c82e032c659fea3580d7
jungbaejeon <wjdqotktls55@gmail.com>
2024-04-27 09:59:03 +0900

    ModelMapper 사용한 Object Copy

---
+++ src/main/java/com/me/designPatterns/_05_prototype/_03_java/GithubIssueData.java
@@ -0,0 +1,50 @@
+package com.me.designPatterns._05_prototype._03_java;
+
+public class GithubIssueData {
+    @Override
+    public String toString() {
+        return "GithubIssueData{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", repositoryUser='" + repositoryUser + '\'' +
+                ", repositoryName='" + repositoryName + '\'' +
+                '}';
+    }
+
+    private int id;
+    private String title;
+    private String repositoryUser;
+    private String repositoryName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRepositoryUser() {
+        return repositoryUser;
+    }
+
+    public void setRepositoryUser(String repositoryUser) {
+        this.repositoryUser = repositoryUser;
+    }
+
+    public String getRepositoryName() {
+        return repositoryName;
+    }
+
+    public void setRepositoryName(String repositoryName) {
+        this.repositoryName = repositoryName;
+    }
+}
